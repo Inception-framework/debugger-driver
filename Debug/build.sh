08526b5d50fade11af017104edfc58d1737b0625
Corteggiani <nassim.corteggiani@maximintegrated.com>
2017-07-10 15:05:17 +0200

    Clean Debug and make build script more configurable

--- Debug/build.sh
+++ Debug/build.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
-name = inception
+name=inception
 
 GCC_VERSION="$(gcc -dumpversion)"
 
 echo "GCC version ${GCC_VERSION} detected"
 
-#make clean
-#if [ $? != 0 ]; then
-#	echo "FAILED! make clean"
-#        exit;
-#fi
+make clean
+if [ $? != 0 ]; then
+	echo "FAILED! make clean"
+        exit;
+fi
+
+rm -rf lib$name.a $name
+
 
 make all
 if [ $? != 0 ]; then
@@ -20,9 +23,9 @@ fi
 
 #libtool --mode=compile g++ -std=c++1y -fpic -I-lpthread -I-lusb-1.0 -O0 -g3 -Wall -c ../src/System.cpp -o src/System.o
 
-find . -name "*.o" -and -not -name "main.o" -print0 | xargs -0 ar crv libinception.a
+find . -name "*.o" -and -not -name "main.o" -print0 | xargs -0 ar crv lib$name.a
 if [ $? != 0 ]; then
-	echo "FAILED! find . -name "*.o" -and -not -name "main.o" -print0 | xargs -0 ar crv libinception.a"
+	echo "FAILED! find . -name "*.o" -and -not -name "main.o" -print0 | xargs -0 ar crv lib$name.a"
         exit;
 fi
 
